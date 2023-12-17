4acc2a18a3683d1905783e0251ad5fdaa442c075
nabielvna <nabielvna@gmail.com>
2023-12-18 05:55:41 +0700

    second

--- App/app.py
+++ App/app.py
@@ -1,10 +1,9 @@
+# app.py
 from flask import Flask, render_template, request
 from astar import astar
+from kecamatan import kecamatan
 
 app = Flask(__name__)
-
-# Contoh data kecamatan dengan informasi tambahan
-from kecamatan import kecamatan
 
 @app.route('/')
 def index():
@@ -17,11 +16,11 @@ def search():
     fuel_price = float(request.form['fuel_price'])
     fuel_efficiency = float(request.form['fuel_efficiency'])
 
-    path = astar(kecamatan, start_location, end_location)
+    result = astar(kecamatan, start_location, end_location)
+    path, total_distance = result
 
     if isinstance(path, list):
-        # Menghitung total jarak, biaya, dan bensin
-        total_distance = sum(kecamatan[kecamatan_path]['neighbors'][kecamatan_path_next] for kecamatan_path, kecamatan_path_next in zip(path, path[1:]))
+        # Menghitung total biaya dan bensin
         total_cost = (total_distance / fuel_efficiency) * fuel_price
         total_fuel_used = total_distance / fuel_efficiency
     else:
@@ -30,7 +29,16 @@ def search():
         total_cost = 0
         total_fuel_used = 0
 
-    return render_template('result.html', start=start_location, end=end_location, path=path, graph=kecamatan, total_distance=total_distance, total_cost=total_cost, total_fuel_used=total_fuel_used)
+    return render_template(
+        'result.html',
+        start=start_location,
+        end=end_location,
+        path=path,
+        graph=kecamatan,
+        total_distance=total_distance,
+        total_cost=total_cost,
+        total_fuel_used=total_fuel_used
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
--- App/astar.py
+++ App/astar.py
@@ -1,3 +1,4 @@
+# astar.py
 def astar(graph, start, end):
     def heuristic(node):
         x1, y1 = graph[node]['coordinates']
@@ -8,6 +9,7 @@ def astar(graph, start, end):
     came_from = {}
     g_score = {start: 0}
     f_score = {start: heuristic(start)}
+    total_distance = {start: 0}
 
     while open_set:
         current = min(open_set, key=lambda node: f_score[node])
@@ -17,7 +19,7 @@ def astar(graph, start, end):
             while current in came_from:
                 path.insert(0, current)
                 current = came_from[current]
-            return path
+            return path, total_distance[end]
 
         open_set.remove(current)
         for neighbor, cost in graph[current]['neighbors'].items():
@@ -26,6 +28,7 @@ def astar(graph, start, end):
                 came_from[neighbor] = current
                 g_score[neighbor] = tentative_g_score
                 f_score[neighbor] = tentative_g_score + heuristic(neighbor)
+                total_distance[neighbor] = total_distance[current] + cost
                 if neighbor not in open_set:
                     open_set.add(neighbor)
 
